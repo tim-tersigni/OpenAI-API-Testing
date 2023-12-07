a74a75667b71c6fbc638323c6a9806b6a38ae8df
Tim Tersigni <tim.tersigni@gmail.com>
2023-12-07 15:58:20 -0600

    Hello world with oph OpenAI API call

---
+++ main.py
@@ -0,0 +1,17 @@
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Loads the variables from .env into os.environ
+
+client = OpenAI()
+
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+  ]
+)
+
+print(completion.choices[0].message)
