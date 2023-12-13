d36afa8d0ada74784d43daada0a6f4cd089d5e47
Tim Tersigni <tim.tersigni@gmail.com>
2023-12-13 16:44:08 -0600

    Create assistant and print test question and response.

--- main.py
+++ main.py
@@ -6,12 +6,46 @@ load_dotenv()  # Loads the variables from .env into os.environ
 
 client = OpenAI()
 
-completion = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
-  ]
+# Upload a file with an "assistants" purpose
+file = client.files.create(
+  file=open("Displacement_and_Gentrification_Recommendation_Inventory.csv", "rb"),
+  purpose='assistants'
 )
 
-print(completion.choices[0].message)
+# Create an assistant with the csv file
+assistant = client.beta.assistants.create(
+    name="Data Analyst",
+    instructions="You are an experienced data analyst studying Austin's Gentrification and Displacement Recommendation dataset. Provide insight and potential questions to explore.",
+    tools=[{"type": "code_interpreter"}],
+    model="gpt-4-1106-preview",
+    file_ids=[file.id]
+)
+
+# Create a new thread
+thread = client.beta.threads.create()
+
+# Add message to thread
+message = client.beta.threads.messages.create(
+    thread_id=thread.id,
+    role="user",
+    content="How many rows are in the dataset?"
+)
+
+# Start a run on the thread
+run = client.beta.threads.runs.create(
+  thread_id=thread.id,
+  assistant_id=assistant.id
+)
+
+while run.status !="completed":
+  run = client.beta.threads.runs.retrieve(
+    thread_id=thread.id,
+    run_id=run.id
+  )
+  print(run.status)
+
+messages = client.beta.threads.messages.list(
+  thread_id=thread.id
+)
+
+print(messages.data[0].content[0].text.value)
